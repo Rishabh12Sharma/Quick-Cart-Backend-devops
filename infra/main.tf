9a3e65c26ed9ae7716507ac661c7c11a44c41831
Rishabh Sharma <rishabhsharmacode@gmail.com>
2026-02-16 17:16:51 +0530

    Add infra

--- infra/main.tf
+++ infra/main.tf
@@ -2,23 +2,48 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Use default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Use default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# IAM role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# EKS cluster
 resource "aws_eks_cluster" "backend_cluster" {
   name     = "backend-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    subnet_ids = data.aws_subnets.default.ids
   }
-}
 
-# Define node group
-resource "aws_eks_node_group" "backend_nodes" {
-  cluster_name    = aws_eks_cluster.backend_cluster.name
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
 }

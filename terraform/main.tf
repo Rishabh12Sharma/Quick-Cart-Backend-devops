5fc30a607ccf1b8f63d93860d73209bc29e8fa0e
Rishabh Sharma <rishabhsharmacode@gmail.com>
2026-02-17 20:27:14 +0530

    Infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,47 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_security_group" "node_sg" {
-  name = "node-sg-terraform"
+data "aws_key_pair" "deployer" {
+  key_name = "deployer-key-terraform-v2"
+}
+
+resource "aws_security_group" "node_sg" {
+  name        = "node-sg-terraform"
+  description = "Allow SSH and Node app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "node_app" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  key_name               = var.key_pair_name
-  vpc_security_group_ids = [data.aws_security_group.node_sg.id]
+  key_name               = data.aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.node_sg.id]
 
   user_data = <<-EOF
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y docker.io
+sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
@@ -25,4 +51,8 @@ EOF
   tags = {
     Name = "Node-App-Server"
   }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.node_app.public_ip
 }

6e0ed1f2c4b71031cecd201e74f3745ce187a360
Rishabh Sharma <rishabhsharmacode@gmail.com>
2026-02-17 21:10:58 +0530

    Infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,51 +2,30 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key-terraform-v2"
-  public_key = var.public_key
+# Use existing key pair (do NOT create)
+data "aws_key_pair" "deployer" {
+  key_name = "deployer-key-terraform-v2"
 }
 
-resource "aws_security_group" "node_sg" {
-  name        = "node-sg-terraform"
-  description = "Allow SSH and Node app"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Use existing security group (do NOT create)
+data "aws_security_group" "node_sg" {
+  name = "node-sg-terraform"
 }
 
 resource "aws_instance" "node_app" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.node_sg.id]
+  key_name               = data.aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [data.aws_security_group.node_sg.id]
 
   user_data = <<-EOF
 #!/bin/bash
-apt update -y
-apt install docker.io -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ubuntu
+sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
 EOF
 
   tags = {

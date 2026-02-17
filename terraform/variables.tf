55876760d87c72d281a6b888702c06bba46ad499
Rishabh Sharma <rishabhsharmacode@gmail.com>
2026-02-17 18:01:23 +0530

    Infra

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "public_key" {
@@ -15,6 +15,3 @@ variable "public_key" {
   type        = string
 }
 
-variable "instance_type" {
-  default = "t3.micro"
-}

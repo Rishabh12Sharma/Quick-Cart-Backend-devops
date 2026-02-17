20f372038b30ac5aaaceda663ad5460273e2b091
Rishabh Sharma <rishabhsharmacode@gmail.com>
2026-02-17 18:31:06 +0530

    Infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,43 +2,22 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key-terraform"   # changed name to avoid duplicate
-  public_key = var.public_key
+# Use existing key pair (do NOT create)
+data "aws_key_pair" "deployer" {
+  key_name = "deployer-key-terraform"
 }
 
-resource "aws_security_group" "node_sg" {
-  name = "node-sg-terraform"   # changed name
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Use existing security group (do NOT create)
+data "aws_security_group" "node_sg" {
+  name = "node-sg-terraform"
 }
 
 resource "aws_instance" "node_app" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  key_name = aws_key_pair.deployer.key_name
-
-  vpc_security_group_ids = [aws_security_group.node_sg.id]
+  key_name = data.aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [data.aws_security_group.node_sg.id]
 
   user_data = <<-EOF
 #!/bin/bash
@@ -52,4 +31,8 @@ EOF
   tags = {
     Name = "Node-App-Server"
   }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.node_app.public_ip
 }
